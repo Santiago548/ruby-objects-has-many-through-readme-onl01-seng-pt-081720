0a9b438a108109bfeec6cdb5d652b5c69d116d70
Dominic Barajas <dominicbarajas548@gmail.com>
2020-09-14 19:22:18 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,18 @@
 class Meal
 
+  attr_accessor :waiter, :customer, :tip, :total
+
+  @@all = []
+
+  def initialize(waiter, customer, tip, total)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
